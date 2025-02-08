013a197391133a08c66f87f79e52a00bd13481b8
PraneshS123 <spranesh09@gmail.com>
2025-02-08 05:19:07 -0500

    footer

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import Header from './app/components/Header';
+import Footer from './app/components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <p>Welcome to the Gamify App!</p>
+      <p>Gamify is an web application that is designed to teach you how financial analysis 
+        and provides an interactive game to help reinforce the lessons you will learn through the tutorial</p>
+      <Footer />
     </div>
   );
 }
---
+++ my-app/src/app/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <button className="footer-button">Tutorial</button>
+      <button className="footer-button">PLAY</button>
+      <button className="footer-button">Leaderboard</button>
+    </div>
+  );
+};
+
+export default Footer;
+
